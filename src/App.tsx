38d2508bb88f6ca6589d0de66db05d82966ea7ce
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-06-21 22:11:38 -0300

    :lipstick: Visual structure of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import { gql, useQuery } from '@apollo/client';
-
-const GET_LESSOS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from './pages/Event';
 
 function App() {
-  const { data, loading, error } = useQuery<{ lessons: Lesson[] }>(
-    GET_LESSOS_QUERY
-  );
-
-  console.log(data);
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
+    <div>
+      <Event />
+    </div>
   );
 }
 
